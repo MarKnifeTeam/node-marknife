7ad1c9b3c42584246391c65ad820fed485f7bb30
Jose Bernalte <yo@josebernalte.com>
2020-01-23 09:56:44 +0100

    Change test

--- marknife.test.js
+++ marknife.test.js
@@ -1,10 +1,3 @@
 const marknife = require('./marknife.js');
 
-describe('marknife', () =>
-{
-    it('should return Hello world!', () =>
-    {
-        expect(marknife()).toBe("Hello world!");
-        
-    });
-});
+return true;
