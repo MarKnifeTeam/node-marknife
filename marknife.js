a64dc42a0036de6e7dcc20bb03f0f21f4b33b1f2
Jose Bernalte <yo@josebernalte.com>
2020-01-23 15:27:35 +0100

    Help

--- marknife.js
+++ marknife.js
@@ -1,4 +1,124 @@
-exports.SayHello = () => 
+const http = require('https');
+
+class Marknife
 {
-    return "Hello world!";
+    constructor(token)
+    {
+        this.token      = token;
+        this.method     = 'GET';
+        this.version    = 1;
+        this.authtype   ='Bearer';
+    }
+
+    SayHello()
+    {
+        return ('Hello world!');
+    }
+
+    Help(options, callback)
+    {
+        this.Read('/help', function(err, data, params)
+        {
+            callback(err, data, params);
+
+        }, options);
+    }
+
+    Read(path, callback, params)
+    {
+        this.method = 'GET';
+        this.Call(path, callback, params);
+    }
+
+    Save(path, callback, params)
+    {
+        this.method = 'POST';
+        this.Call(path, callback, params);
+    }
+
+    Delete(path, callback, params)
+    {
+        this.method = 'DELETE';
+        this.Call(path, callback, params);
+    }
+
+    Update(path, callback, params)
+    {
+        this.method = 'PUT';
+        this.Call(path, callback, params);
+    }
+
+    Patch(path, callback, params)
+    {
+        this.method = 'patch';
+        this.Call(path, callback, params);
+    }
+
+    Purge(path, callback, params)
+    {
+        this.method = 'PURGE';
+        this.Call(path, callback, params);
+    }
+
+    Call(path, callback, params)
+    {
+    	var req = null;
+        
+        if(typeof this.token == 'undefined' ||  this.token == '')
+            return callback(new Error('Marknife Invalid Token'));
+
+        var bearer = this.authtype + ' ' + this.token;
+
+        var headers = {};
+        if(bearer.length > 0) headers['Authorization'] = bearer;
+        if(params)
+        {
+            params = {params : params};
+            headers['Content-Type'] = 'application/json';
+            headers['Content-Length'] = Buffer.byteLength(JSON.stringify(params));
+        }
+
+        if (!req)
+    	{
+            var path = '/help';
+
+            var postRequest =
+            {
+                host: 'api.marknife.com',
+                path: '/v' + this.version + path,
+                port: 443,
+                method: this.method,
+                headers: headers
+            };
+            var buffer = "";
+
+            const req = http.request( postRequest, (res) =>
+            {
+                var buffer = "";
+                res.on( "data", data  =>
+                {
+                    buffer = buffer + data;
+                });
+                res.on("end", data =>
+                {
+                    if (callback && typeof callback === 'function')
+                    {
+                        buffer = JSON.parse(buffer);
+                        callback(null, buffer, params);
+                    }
+                });
+            });
+
+            req.on('error', function(err)
+            {
+                console.log(err);
+                throw err;
+            });
+
+            if(params) req.write(JSON.stringify(params));
+            req.end();
+        }
+    }
 }
+
+module.exports = Marknife;
--- marknife.test.js
+++ marknife.test.js
@@ -1,3 +1,35 @@
-const marknife = require('./marknife.js');
+const Marknife  = require('./marknife');
 
-if(marknife.SayHello() != 'Hello world!') throw new Error('SayHello');
+var token   = 'aa';
+var m       = new Marknife(token);
+
+var p = [];
+
+    p.push(new Promise(function(resolve, reject)
+    {
+        if(m.SayHello() == 'Hello world!') return resolve();
+        reject(new Error('SayHello'));
+    }));
+
+    //HELP
+    p.push(new Promise(function(resolve, reject)
+    {
+        m.Help(null, function(err, data, params)
+        {
+            if(err || data.application.name != 'Marknife')
+            {
+                reject(new Error('Help'));
+            }
+            return resolve();
+        });
+    }));
+
+Promise.all(p).then(function(r)
+{
+    console.log("Ok!");
+
+}).catch(function(err)
+{
+    console.log(err);
+    throw err;
+});
